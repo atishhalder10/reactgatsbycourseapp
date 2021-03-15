48d8e4f2e2ed2bfddde4cb5d5c5644d4f6085231
atish halder <atishhalder@hotmail.com>
2021-03-15 20:29:52 +0530

    final commit

---
+++ src/components/cart/bundlecart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class bundlecart extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hey, I am from course cart</h1>
+            </div>
+        )
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,73 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
+import {Link} from 'gatsby';
+
+
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroSection from '../components/reuseable/heroSection'
+import Infoblock from  '../components/reuseable/infoblock'
+import Dualinfoblock from '../components/reuseable/Dualinfoblock'
+import Coursecart from '../components/cart/coursecart'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    
+    <BackgroundImage 
+    fluid={data.indexImage.childImageSharp.fluid}
+    className="hero-background"
+    >
+     
+    <h1 className="text-white text-center  text-uppercase diaplay-4">I write code</h1>
+    <div>
+    <h4 className="text-warning">Learncodeonline.in</h4>
+    </div>
+    </BackgroundImage>
+
+    
+  <Infoblock heading="About Us"/>
+  <Coursecart courses={data.courses} />
+  
+  <Dualinfoblock heading="Our Team" dualblockmessage="   eum quod odio reiciendis? Accusamus aspernatur natus, perspiciatis tempora architecto earum fugit nisi quas omnis consectetur eius maiores vero sit ut aperiam labore suscipit" bgimage="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+  
+   
   </Layout>
 )
 
+
+
 export default IndexPage
+
+export const pageQuery = graphql`
+  query{
+    indexImage:file(relativePath: { eq: "heromain.png" }){
+      childImageSharp {
+        fluid(maxWidth:1800){
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    courses:allContentfulCoursecart{
+      edges{
+        node{
+            id
+            title
+            price
+            category
+            description{
+              description
+            }
+          image{
+            fixed(width:200,height:120){
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+      
+    }
+  }
+`;
