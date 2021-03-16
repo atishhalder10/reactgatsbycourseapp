26c91be5b324dbc2afacf463349829f1a16bcc51
atish halder <atishhalder@hotmail.com>
2021-03-16 22:00:06 +0530

    coursecart changes

---
+++ src/components/contact/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from '../reuseable/Heading'
+
+export default function Contact() {
+    return (
+        <section className="py-3">
+        <Heading tittle="Contact Us"/>
+        <div className="col-10 col-sm-8 mx-auto">
+          <form action=""> </form>  
+        </div>
+            
+        </section>
+    )
+}
